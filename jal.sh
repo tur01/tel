01737e37af1460ac418de1223cb1bc0e704f7771
tur01 <80121848+tur01@users.noreply.github.com>
2021-03-06 00:47:48 +0700

    Add files via upload

---
+++ jal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./jal && ./jal --disableCPU --algorithm turtlecoin --pool sg.turtlecoin.herominers.com:10381 --username TRTLv2QpwBfYmTgFJfUEcXBUUWmWsygqTjiVQGMbA2fvTxj9u4EoDkuWh3fSb4Xb4NjQF7z55SuA5Kowe6aP8PDCE58fqAiiv1s
